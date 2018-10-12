16e7fbc90c4ae5af97759890dafd4deac1e3d838
smailik120 <33979848+smailik120@users.noreply.github.com>
2018-10-12 10:45:39 +0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class  HelloWorld{
     public static void main(String[] args) {
         //Prints "Hello, World" to the terminal window
-        System.out.println("Hello World from bracnh 2");
+        System.out.println("Hello World from bracnh2");
     }
 }
